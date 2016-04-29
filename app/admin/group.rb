5da106baefb77438c931808fab63d05a52ff15f4
Prelang Builder <builder@prelang.com>
2016-04-29 06:27:17 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Group' to allow for create, update

--- app/admin/group.rb
+++ app/admin/group.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Group do
+  permit_params :gid, :name, :creator, :description, :privacy, :website, :email, :icon50, :icon
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
